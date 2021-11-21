2220b7064d0cc8a1c9eaeac04268a2888946463a
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-11-21 03:08:02 +0100

    feat : React Component Creation

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate that you need to bundle `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <div className="main-view">
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
