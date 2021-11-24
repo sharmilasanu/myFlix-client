c0c6142890a7da71907eb277d33e4d3d9aeed4e8
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-11-24 05:08:13 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/MainView/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,11 +9,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="main-view">
-        <div>Inception</div>
-        <div>The Shawshank Redemption</div>
-        <div>Gladiator</div>
-      </div>
+      <MainView />
     );
   }
 }
