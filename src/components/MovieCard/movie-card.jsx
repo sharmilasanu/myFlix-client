4608076e135139326c035a0531120740fa46e97b
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-25 16:15:53 +0100

    Update movie-card.jsx

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,34 +1,31 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+// react-bootstrap UI
+import { Card, Button } from 'react-bootstrap';
 
-export class MovieCard extends React.Component{
-
-    render() {
-        const { movie, onMovieClick } = this.props;
-        return (
-          
-          <Card className = 'moviecard'>
+export function MovieCard({ movie }) {
+    return (
+        <Card>
             <Card.Img variant="top" src={movie.ImagePath} />
             <Card.Body>
-              <Card.Title>{movie.Title}</Card.Title>
-              <Card.Text>{movie.Description}</Card.Text>
-              <Button onClick={() => onMovieClick(movie)} variant="link">More Details</Button>
+                <Card.Title style={{ fontWeight: 700, fontSize: "20px" }}>{movie.Title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "20px" }}>{movie.Director.Name}</Card.Subtitle>
+                <Link to={`/movies/${movie._id}`} >
+                    <Button variant="info" className="mt-4">Open details</Button>
+                </Link>
             </Card.Body>
-          </Card>
-        );
-      }
-      
+        </Card>
+    );
 }
 
-
 MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-};
+    movie: PropTypes.shape({
+      ImagePath: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired
+        }).isRequired,
+        Title: PropTypes.string.isRequired
+    }).isRequired
+}
