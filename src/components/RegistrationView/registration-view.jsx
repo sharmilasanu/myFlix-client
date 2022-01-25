ea21fa8c90ba249f02eae3e6a9b4b8fb25b6bd82
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-25 16:14:50 +0100

    New Users Registration Component

--- src/components/RegistrationView/registration-view.jsx
+++ src/components/RegistrationView/registration-view.jsx
@@ -1,63 +1,78 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
-export function RegistrationView(props) {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ birthday, setBirthday ] = useState('');
-    const [ email, setEmail ] = useState('');
+// react-bootstrap UI
+import { Form, FloatingLabel, Button } from 'react-bootstrap';
+// scss file
+import './registration-view.scss';
+// logo img
+
+
+export function RegistrationView() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
-      if (username.length < 4) return setError('Must include a username that is longer than 4 characters');
-      if (password.length < 6) return setError('Must include a password that is longer than 6 characters');
-      const alphaNum = /^[0-9a-zA-Z]+$/;
-      if (!username.match(alphaNum)) return setError('Username must contain only letters and numbers');
-      if (password !== confirmPassword) return setError('Passwords do not match');
+        e.preventDefault();
 
-      axios.post('https://avengers-database.herokuapp.com/users/', {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday
-      })
-          .then(response => {
-              console.log(response.data);
-              window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
-          })
-          .catch(e => {
-              setError('Username already exists please pick another one')
-              console.log('error registering the user')
-          });
-    };
+        // error validation
+        if (username.length < 4) return setError('Must include a username that is longer than 4 characters');
+        if (password.length < 6) return setError('Must include a password that is longer than 6 characters');
+        const alphaNum = /^[0-9a-zA-Z]+$/;
+        if (!username.match(alphaNum)) return setError('Username must contain only letters and numbers');
+        if (password !== confirmPassword) return setError('Passwords do not match');
+
+        axios.post('https://sharmismyflix.herokuapp.com/users/', {
+            UserName: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        })
+            .then(response => {
+                console.log(response.data);
+                window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+            })
+            .catch(e => {
+                setError('Username already exists please pick another one')
+                console.log('error registering the user')
+            });
+    }
+
     return (
-
-        <Col sm = {5} >
-        <Form >
-          <h3>Registration</h3>
-          <Form.Group controlId="formUsername">
-            <Form.Label>Username :</Form.Label>
-            <Form.Control type="text" placeholder = "Enter First Name" onChange={e => setUsername(e.target.value)} />
-           </Form.Group>
-
-          <Form.Group controlId="formPassword">
-            <Form.Label>Password :</Form.Label>
-            <Form.Control type="text" placeholder = "Should Contain minimun 8 Charchters" onChange={e => setPassword(e.target.value)} />
-           </Form.Group>
-    
-          <Form.Group controlId="formEmail">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control type="text"  placeholder = "Enter valid Email Address" onChange={e => setEmail(e.target.value)} />
-          </Form.Group>
-
-          <Form.Group controlId="formBirthday">
-            <Form.Label>Date of Birth:</Form.Label>
-            <Form.Control type="text"  onChange={e => setBirthday(e.target.value)} />
-          </Form.Group>
-
-          <Button className="btn btn-dark btn-lg btn-block" type="submit" onClick={handleSubmit}>
-            Submit
-          </Button>
-          <Button variant="secondary" type="button">Register</Button>
-        </Form>
-       </Col>
-      );
+        <div className="mt-3 d-flex flex-column justify-content-center align-items-center">
+            <h1 className="d-flex align-items-center mb-5" style={{ fontFamily: 'Montserrat', fontWeight: 700, color: "#777978" }}>
+               
+            </h1>
+            <Form className="registration-view" onSubmit={handleSubmit} style={{ textAlign: "center" }}>
+                <h1 style={{ fontFamily: 'Montserrat', fontWeight: 700 }}>Register</h1>
+                <FloatingLabel controlId="formUsername" label="Username*" className="mb-3 mt-4">
+                    <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="username" required />
+                </FloatingLabel>
+                <FloatingLabel controlId="formPassword" label="Password*" className="mb-3">
+                    <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="password" required />
+                </FloatingLabel>
+                <FloatingLabel controlId="formConfirmPassword" label="Confirm Password*" className="mb-3">
+                    <Form.Control type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="confirmPassword" required />
+                </FloatingLabel>
+                <FloatingLabel controlId="formEmail" label="Email*" className="mb-3">
+                    <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" required />
+                </FloatingLabel>
+                <FloatingLabel controlId="formBirthday" label="Birthday" className="mb-3">
+                    <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="birthday" />
+                </FloatingLabel>
+                {error && <h5 style={{ color: "red", marginBottom: "40px" }}>{error}</h5>}
+                <div className="d-grid gap-2">
+                    <Button size="lg" variant="success" type="submit">Submit</Button>
+                </div>
+                <Link to={`/`}>
+                    <Button size="lg" variant="primary" className="login-button">Login</Button>
+                </Link>
+            </Form>
+        </div>
+    )
 }
