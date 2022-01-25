1974c63adb9c41b3991ae1c28011f60ff03d7602
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-25 16:13:01 +0100

    Navbar Component

---
+++ src/components/topbar/topbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+// react-bootstrap UI
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+
+// logo img
+import img from "../../logo.png";
+
+export function Topbar({ onLoggedOut }) {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Link to={`/`} style={{ textDecoration: 'none' }}>
+          <Navbar.Brand href="#home" className="d-flex align-items-center" style={{ fontFamily: 'Montserrat', fontWeight: 700, color: "rgb(149,149,149)" }}>
+           
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-center" style={{ width: "100%" }}>
+            <Nav.Link as={Link} to="/profile" style={{ fontSize: "20px" }}>Profile</Nav.Link>
+          </Nav>
+          <Link to={'/'}>
+            <Button variant="danger" onClick={onLoggedOut} style={{ width: "90px" }}>Log out</Button>
+          </Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+Topbar.propTypes = {
+  onLoggedOut: PropTypes.func.isRequired
+}
