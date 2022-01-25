12b030bb8093a9bcbcaf1312c21ce4eae5127dcd
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-25 16:16:45 +0100

    Redux implementation

---
+++ src/actions/actions.js
@@ -0,0 +1,27 @@
+
+// actions 
+export const SET_MOVIES = "SET_MOVIES";
+export const SET_FILTER = "SET_FILTER";
+export const SET_USER = "SET_USER";
+
+// action creaters 
+export function setMovies(value) {
+    return {
+        type: SET_MOVIES,
+        value
+    }
+}
+
+export function setFilter(value) {
+    return {
+        type: SET_FILTER,
+        value
+    }
+}
+
+export function setUser(value) {
+    return {
+        type: SET_USER,
+        value
+    }
+}
